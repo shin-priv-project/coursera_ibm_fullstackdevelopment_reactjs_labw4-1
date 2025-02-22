d4eda20ba30362e52943448a6bfeadf908e64ffd
Shin <basket0903@gmail.com>
2025-02-22 15:48:28 +0900

    Set up gh-pages.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/"
+  base: "/coursera_ibm_fullstackdevelopment_reactjs_labw4-1",
   plugins: [react()],
 })
